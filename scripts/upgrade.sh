f57cafe2543c943434d93bea4b725d154c3c353b
Laurent Laville <laurent.laville@gmail.com>
2020-07-02 13:25:13 +0200

    fix tar extrac command and add shebang

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 # Docker compose upgrading script
 # Execute this script with command
 #   make mk-upgrade
@@ -6,5 +8,5 @@ RELEASE=https://github.com/$MK_REPO/archive/$MK_VERSION.tar.gz
 
 # Downloading new release
 echo "Downloading version $MK_VERSION from $RELEASE"
-curl -Ls $RELEASE | tar zxfm -
+curl -Ls $RELEASE | tar zxfm - devilbox-docker-compose-makefile-$MK_VERSION/.mk-lib --strip-components=1
 echo "Done"
